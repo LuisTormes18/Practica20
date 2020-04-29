4413928bddea4104174bd204a3f58347948cf1a5
LuisTormes <luistormes.lata@gmail.com>
2020-04-29 14:01:46 -0400

    avance wn la capa de datos

--- Practica20_git_c-shar_SQL-server/Capa_Datos/Parametros.cs
+++ Practica20_git_c-shar_SQL-server/Capa_Datos/Parametros.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data;
+using System.Data.SqlClient;
 namespace Capa_Datos
 {
-    class Parametros
+    public class Parametros
     {
+        /*atributos*/
+        public string nombreP { get; set; }
+        public SqlDbType valor { get; set; }
+
+        /*Contructores*/
+
+            /*C.Parametros de entrada*/
+
+            /*C.parametros de salida*/
     }
 }
--- Practica20_git_c-shar_SQL-server/Capa_Datos/Sql_Connet.cs
+++ Practica20_git_c-shar_SQL-server/Capa_Datos/Sql_Connet.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data;
+using System.Data.SqlClient;
 namespace Capa_Datos
 {
     class Sql_Connet
     {
+        private SqlConnection conexion = new SqlConnection("Data Source =PC-PC\\SQLEXPRESS ; Initial Catalog =Practica;Integrated Security=true");
+
+        /*metodo para abrir la coneccion*/
+        public void abrir_conexion()
+        {
+            if(conexion.State == ConnectionState.Closed)
+            {
+                conexion.Open();
+            }
+        }
+
+        /*metodo para cerrar la coneccion*/
+        public void cerrar_conexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+        }
     }
 }
